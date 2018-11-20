7fcabcf033ea7904d698ef960941e0cb328ec94d
Alex75Git <45144806+Alex75Git@users.noreply.github.com>
2018-11-20 13:04:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/alexandr/mytestgit2/MainActivitytTestGit2.kt
@@ -0,0 +1,15 @@
+package com.example.alexandr.mytestgit2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivitytTestGit2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activityt_test_git2)
+
+        // commit 1
+
+    }
+}
